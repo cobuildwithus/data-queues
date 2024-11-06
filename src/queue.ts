9e44cf0ea1fae658a649b431775f8ed9f93d23b0
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-05 22:30:13 -0500

    new version!

--- src/queue.ts
+++ src/queue.ts
@@ -7,7 +7,7 @@ import { embeddings } from './database/schema';
 import { db } from './database/db';
 import { and, eq } from 'drizzle-orm';
 
-const version = 15;
+const version = 16;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY as string,
