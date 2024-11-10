709921f36bafcdc8c71196379535eba40e68ee9a
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-10 15:20:55 -0500

    Update queue.ts

--- src/queue.ts
+++ src/queue.ts
@@ -27,9 +27,6 @@ const connection: ConnectionOptions = {
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
-  // socket: {
-  //   family: 6,
-  // },
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
